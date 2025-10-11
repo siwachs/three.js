02f2e7cc7fd7bbf20a7c74c009fce66e4e67a66d
siwachs <ssiwach.8888@gmail.com>
2025-10-11 12:07:52 +0530

    Get started with gsap

--- gsap-cc-starter/src/pages/GsapFrom.jsx
+++ gsap-cc-starter/src/pages/GsapFrom.jsx
@@ -1,5 +1,17 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
-  // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 350,
+      repeat: -1,
+      yoyo: true,
+      rotate: 360,
+      duration: 2,
+      ease: "power1.inOut",
+    });
+  }, []);
 
   return (
     <main>
--- gsap-cc-starter/src/pages/GsapText.jsx
+++ gsap-cc-starter/src/pages/GsapText.jsx
@@ -1,5 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
-  // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to("#text", {
+      opacity: 1,
+      ease: "power1.inOut",
+      y: 0,
+    });
+
+    gsap.fromTo(
+      ".para",
+      {
+        opacity: 0,
+        y: 0,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        stagger: 0.1,
+      }
+    );
+  }, []);
 
   return (
     <main>
